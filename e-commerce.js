e934dec02d38cea35d7f534eb1cbd93ea9f4aa55
Arpan <arpangupta762@gmail.com>
2024-09-25 21:08:44 +0530

    add some js and change HTML

---
+++ e-commerce.js
@@ -0,0 +1,65 @@
+let products = [
+    {
+        id : 1,
+        name : "Laptop",
+        category : "Computers",
+        brand : "ExampleBrand",
+        price : 799.99,
+        stock : 50,
+        description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ipsum?",
+        image : "gaming laptop.png"
+    },
+    {
+        id : 2,
+        name : "Smartphone",
+        category : "Mobiles",
+        brand : "TechGadget",
+        price : 499.99,
+        stock : 100,
+        description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ipsum?",
+        image : "s24.png"
+    },
+    {
+        id : 3,
+        name : "Wireless Headphones",
+        category : "Audio",
+        brand : "SoundBeats",
+        price : 149.99,
+        stock : 30,
+        description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ipsum?",
+        image : "headphone1.png"
+    },
+    {
+        id : 4,
+        name : "Watches",
+        category : "Wearables",
+        brand : "FitTech",
+        price : 199.99,
+        stock : 20,
+        description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ipsum?",
+        image : "smartwatch.png"
+    },
+    {
+        id : 5,
+        name : "Speaker",
+        category : "Audio",
+        brand : "SoundBeats",
+        price : 199.99,
+        stock : 30,
+        description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ipsum?",
+        image : "speaker.png"
+    },
+    {
+        id : 6,
+        name : "T.V",
+        category : "Video",
+        brand : "Samsung",
+        price : 999.99,
+        stock : 12,
+        description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ipsum?",
+        image : "samsung tv.png"
+    }
+]; 
+
+const productContainer = document.querySelector(".products-container");
+const productTemplate = document.querySelector(".productTemplate");
